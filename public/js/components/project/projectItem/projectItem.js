a2eac6b0f3874cc26e647c582bad449991d3e2c6
llTonyll <tonygomhh@gmail.com>
2016-11-30 10:03:29 +0100

    fix,back

--- public/js/components/project/projectItem/projectItem.js
+++ public/js/components/project/projectItem/projectItem.js
@@ -1,34 +1,15 @@
 ((app) => {
     'use strict'
     app.component('projectItem', {
+        bindings: {
+            post: "<",
+        },
         templateUrl: 'js/components/project/projectItem/projectItem.html',
-        controller: ['$scope', function($scope) {
-            this.sites = [{
-                'name': 'Google',
-                'link': 'http://www.google.com',
-                'author': 'Jon Snow',
-                'src': 'https://blog.validately.com/wp-content/uploads/2015/03/Screen-Shot-2015-03-10-at-3.54.21-PM.png'
-            }, {
-                'name': 'Facebook',
-                'link': 'http://www.facebook.com',
-                'author': 'Marc Zuckerberg',
-                'src': 'http://1.bp.blogspot.com/-gW1ucleeuXI/T2n7kVs57cI/AAAAAAAABpA/bEIywU6kqBc/s1600/NewsFeed1.png'
-            }, {
-                'name': 'Metalorgie',
-                'link': 'http://www.metalorgie.com/',
-                'author': 'Lemmy Killimister',
-                'src': 'http://www.pixbear.com/wp-content/uploads/2011/05/stupeflip_featured.jpg'
-            }, {
-                'name': 'NASA',
-                'link': 'http://www.nasa.com/',
-                'author': 'les teletubbies',
-                'src': 'http://www.pixbear.com/wp-content/uploads/2011/05/stupeflip_featured.jpg'
-            }, {
-                'name': 'Ghost',
-                'link': 'http://www.ghost.com/',
-                'author': 'Ghost Rider',
-                'src': 'http://www.pixbear.com/wp-content/uploads/2011/05/stupeflip_featured.jpg'
-            }]
+        controller: ['ProjectsService', '$stateParams', '$state', '$http', function(ProjectsService, $stateParams, $state, $http) {
+
+          $http.get("sites.json").then((read) => {
+          this.projects = read.data
+      })
         }]
     })
 })(require('angular').module('app.project'))
