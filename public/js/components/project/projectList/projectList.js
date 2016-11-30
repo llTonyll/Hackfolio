e4e0ad26e5f2694c99823aade086ab8e8cbc2ad2
Bully47 <vivien.prog@gmail.com>
2016-11-30 10:32:56 +0100

    before retro

--- public/js/components/project/projectList/projectList.js
+++ public/js/components/project/projectList/projectList.js
@@ -5,38 +5,45 @@ Create Angular component projectList into module app.project
     'use strict'
     app.component('projectList', {
         templateUrl: 'js/components/project/projectList/projectList.html',
-        controller:['$scope', function($scope) {
+        controller: ['$scope', function($scope) {
             $scope.sites = [{
                 'name': 'Google',
                 'link': 'http://www.google.com',
-                'author': 'Jon Snow',
+                'author': 'Sundar Pichai',
                 'src': 'https://blog.validately.com/wp-content/uploads/2015/03/Screen-Shot-2015-03-10-at-3.54.21-PM.png'
             }, {
                 'name': 'Facebook',
                 'link': 'http://www.facebook.com',
                 'author': 'Marc Zuckerberg',
-                'src': 'http://1.bp.blogspot.com/-gW1ucleeuXI/T2n7kVs57cI/AAAAAAAABpA/bEIywU6kqBc/s1600/NewsFeed1.png'
+                'src': 'http://image.noelshack.com/fichiers/2016/48/1480491777-facebook.png'
             }, {
-                'name': 'Metalorgie',
-                'link': 'http://www.metalorgie.com/',
-                'author': 'Lemmy Killimister',
-                'src': 'http://www.pixbear.com/wp-content/uploads/2011/05/stupeflip_featured.jpg'
+                'name': 'Twitter',
+                'link': 'http://www.twitter.com/',
+                'author': 'Jack Dorsey',
+                'src': 'http://image.noelshack.com/fichiers/2016/48/1480491767-twitter.png'
             }, {
-                'name': 'NASA',
-                'link': 'http://www.nasa.com/',
-                'author': 'les teletubbies',
-                'src': 'http://www.pixbear.com/wp-content/uploads/2011/05/stupeflip_featured.jpg'
+                'name': 'Tumblr',
+                'link': 'http://www.tumblr.com/',
+                'author': 'David Karp',
+                'src': 'http://image.noelshack.com/fichiers/2016/48/1480491719-tumblr.png'
             }, {
-                'name': 'Ghost',
+                'name': 'Youtube',
+                'link': 'http://www.youtube.com/',
+                'author': 'Susan Wojcicki',
+                'src': 'http://image.noelshack.com/fichiers/2016/48/1480491796-youtube.png'
+            }, {
+                'name': 'Dailymotion',
+                'link': 'http://www.dailymotion.com/',
+                'author': 'Maxime Saada',
+                'src': 'http://image.noelshack.com/fichiers/2016/48/1480491782-dailymotion.png'
+            }, {
+                'name': 'Pole-emploi',
                 'link': 'http://www.ghost.com/',
-                'author': 'Ghost Rider',
+                'author': 'Jean Bassères',
                 'src': 'http://www.pixbear.com/wp-content/uploads/2011/05/stupeflip_featured.jpg'
             }]
-            this.add = () => {
-                this.site.push(this.site);
-            };
             this.delete = (index) => {
-                this.site.splice(index, 1);
+                this.posts.splice(index, 1);
             };
         }]
     })
--- public/js/services/projectsService.js
+++ public/js/services/projectsService.js
@@ -1,30 +1,30 @@
 /*
-Create Angular service PostsService in app.services module
+Create Angular service ProjectsService in app.services module
 */
 ((app) => {
     'use strict'
-    app.service('PostsService', function($http) {
+    app.service('ProjectsService', function($http) {
         return {
             get() {
                 // HTTP Request method GET to our express API
-                return $http.get('/api/posts')
+                return $http.get('/api/projects')
             },
             getById(id) {
                 // HTTP Request method GET with param (post id) to our express API
-                return $http.get('/api/posts/' + id)
+                return $http.get('/api/projects/' + id)
             },
             save(post) {
                 if (post._id) {
                     // HTTP Request method PUT (update) with param and data (post) to our express API
-                    return $http.put('/api/posts/' + post._id, post)
+                    return $http.put('/api/projects/' + post._id, post)
                 } else {
                     // HTTP Request method POST (create) with data (post) to our express API
-                    return $http.post('/api/posts', post)
+                    return $http.post('/api/projects', post)
                 }
             },
             delete(post) {
                 // HTTP Request method DELETE (delete) with param (post id) to our express API
-                return $http.delete('/api/posts/' + post._id)
+                return $http.delete('/api/projects/' + post._id)
             }
         }
     })
